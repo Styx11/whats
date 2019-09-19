21401a3010bc65b1b732a35b1bdc95fc910ed424
styx11 <1654630248@qq.com>
2019-09-19 20:22:58 +0800

    :sparkles: update index with commander

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-const http = require('http');
-const xml2js = require('xml2js');
+#!/usr/bin/env node
+const iciba = require('./lib/iciba');
+const commander = require('commander');
+const program = new commander.Command();
+const { checkVers } = require('./lib/util/checkVers');
+const { checkLang } = require('./lib/util/checkLang');
+
+const pkg = require('./package.json');
+const version = pkg.version;
+
+program.version(version);
+
+program
+  .name('whats')
+  .usage('<word> [options]')
+  .option('-f, --from <source>', 'the source language to translate')
+  .option('-t, --to <target>', 'the target language');
+
+program.on('--help', () => {
+  console.log('')
+  console.log('Examples:');
+  console.log('  $ whats love');
+  console.log('  $ whats 爱');
+  console.log('  $ whats l’amour --from fr --to zh-CHS');
+  console.log('');
+})
+
+if (!process.argv.slice(2).length) {
+  console.log('');
+  return program.help();
+}
+
+program.parse(process.argv);
+
+if (!checkVers()) {
+  return;
+}
+
+let to = program.to;
+let from = program.from;
+const word = process.argv[2].toLocaleLowerCase();
+const useIciba = checkLang(word, from, to);
+if (useIciba) {
+  iciba(word);
+} else {
+  from = from ? from : 'auto';
+  to = to ? to : 'auto';
+}
